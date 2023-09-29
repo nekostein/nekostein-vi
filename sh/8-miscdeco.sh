2f072645d217e19c29c1bf49f141957876c335c9
Neruthes <i@neruthes.xyz>
2023-09-29 14:01:00 +0000

    Regular update '2023-09-29T14:01:00+00:00'

--- sh/8-miscdeco.sh
+++ sh/8-miscdeco.sh
@@ -9,15 +9,15 @@ mkdir -p "$destdir"
 function makeDeco1_cropTR() {
     color="$1"
     width_old=6400
-    width_new=2400
-    height_new=1500
+    width_new=2100
+    height_new=1100
     imgsrc="_dist/wwwmisc/geologo/Nekostein-geologo.${color}_null.8x.png"
     sizespec="${width_new}x${height_new}+$((width_old-width_new))+0"
     tmp=".tmp/cropped_Logo.$color.png"
     convert "$imgsrc" -crop "$sizespec" +repage "$tmp"
     convert -size "${width_new}x${height_new}" xc:none "$tmp-canvas.png"
-    convert "$tmp" -channel A -fx 'a*0.2' "$tmp-20.png"
-    composite -gravity center "$tmp-20.png" "$tmp-canvas.png" "$destdir/geodeco-crop.tr.$color.png"
+    convert "$tmp" -channel A -fx 'a*0.05' "$tmp-lower.png"
+    composite -gravity center "$tmp-lower.png" "$tmp-canvas.png" "$destdir/geodeco-crop.tr.$color.png"
     echo "$destdir/geodeco-crop.tr.$color.png"
     rm "$tmp" "$tmp-"*
 
